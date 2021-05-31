25908139a5f2542c60100f6cfe9b12edf9deacc8
Wesley Guerra <guerra_ssa@hotmail.com>
2021-05-30 21:10:23 -0300

    Add styled global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import GlobalStyle from "./styles/global";
 
 function App() {
-  return <h1>Test</h1>;
+  return (
+    <>
+      <h1>Test</h1>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
